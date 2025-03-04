316c25d20f6d7e58034926b7c4b51fb99205eaca
s1ddhants1 <siddhant2257@gmail.com>
2025-03-05 03:33:43 +0530

    27

--- src/lib/components/Valentine.svelte
+++ src/lib/components/Valentine.svelte
@@ -33,7 +33,7 @@
   <div class="typing-animation">
     <p class="text-3xl mt-2 text-white-800 dark:text-white-300">heyy nini!!</p>
     <p class="text-3xl mt-2 text-white-800 dark:text-white-300">Would you like to...</p>
-    <p class="text-6xl leading-20 bg-gradient-to-r from-pink-500 via-pink-700 to-pink bg-clip-text text-transparent font-bold">
+    <p class="text-6xl leading-20 bg-gradient-to-r from-pink-700 via-pink-500 to-pink-200 bg-clip-text text-transparent font-bold">
       Be My Valentine?
     </p>
     <p class="text-3xl mt-2 text-white-800 dark:text-white-300">...please?</p>
