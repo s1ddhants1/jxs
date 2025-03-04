8010725d65fa2b072f914c27e282fe8293503dd0
s1ddhants1 <siddhant2257@gmail.com>
2025-03-05 03:20:17 +0530

    22

--- src/lib/components/Valentine.svelte
+++ src/lib/components/Valentine.svelte
@@ -34,12 +34,12 @@
 <!-- Valentine's Message -->
 <div class="w-full mt-50 dark:text-white text-center"> 
   <div class="typing-animation">
-    <p class="text-4xl mt-2 text-white-800 dark:text-white-300">heyy nini!!</p>
-    <p class="text-4xl mt-2 text-white-800 dark:text-white-300">Would you like to...</p>
+    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">heyy nini!!</p>
+    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">Would you like to...</p>
     <p class="text-6xl leading-20 bg-gradient-to-r from-pink-700 via-pink-500 to-pink bg-clip-text text-transparent font-bold">
       Be My Valentine?
     </p>
-    <p class="text-4xl mt-2 text-white-800 dark:text-white-300">...please?</p>
-    <p class="text-4xl mt-2 text-white-800 dark:text-white-300">I love you</p>
+    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">...please?</p>
+    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">I love you</p>
   </div>
 </div>
