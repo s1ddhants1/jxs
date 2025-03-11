912c39fdcf52706c4a7b4bf577d7ebea88bdcf08
s1ddhants1 <siddhant2257@gmail.com>
2025-03-11 16:12:37 +0530

    35

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,8 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
 <footer class="bg-neutral-900 text-white text-center py-4 mt-auto">
-    <p class="text-sm">&copy; {new Date().getFullYear()} JxS. All rights reserved.</p>
+    <p class="text-sm">&copy; {year} JxS. All rights reserved.</p>
   </footer>
   
