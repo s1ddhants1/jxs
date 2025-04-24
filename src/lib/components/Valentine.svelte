095a974294513bf6777022396e58e08c5bfff24c
s1ddhants1 <siddhant2257@gmail.com>
2025-04-24 22:28:40 +0530

    fix page text optics

--- src/lib/components/Valentine.svelte
+++ src/lib/components/Valentine.svelte
@@ -1,6 +1,4 @@
 <script>
-  let lines = [];
-  let delay = 0;
 </script>
 
 <div class="fixed inset-0 z-10 overflow-hidden">
@@ -10,14 +8,14 @@
   </video>
 </div>
 
-<div class="font-[LondonBetween] z-20 w-full mt-50 dark:text-white text-center"> 
+<div class="font-[LondonBetween] flex mt-30 md:mt-40 z-20 w-full dark:text-white items-center justify-center"> 
   <div class="typing-animation">
-    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">heyy nini!!</p>
-    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">Would you like to...</p>
-    <p class="text-6xl leading-20 bg-gradient-to-r from-pink-700 via-pink-500 to-pink-200 bg-clip-text text-transparent font-bold">
+    <p class="text-xl md:text-3xl mt-2 text-white-800 dark:text-white-300">heyy nini!!</p>
+    <p class="text-xl md:text-3xl mt-2 text-white-800 dark:text-white-300">would you like to...</p>
+    <p class="text-4xl md:text-6xl leading-20 bg-gradient-to-r from-pink-700 via-pink-500 to-pink-200 bg-clip-text text-transparent font-bold">
       Be My Valentine?
     </p>
-    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">...please?</p>
-    <p class="text-3xl mt-2 text-white-800 dark:text-white-300">I love you</p>
+    <p class="text-xl md:text-3xl mt-2 text-white-800 dark:text-white-300">...please?</p>
+    <p class="text-xl md:text-3xl mt-2 text-white-800 dark:text-white-300">I love you &lt;3</p>
   </div>
 </div>
