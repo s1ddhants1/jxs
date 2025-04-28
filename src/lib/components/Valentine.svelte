1d0bc4724149800dcf1cd5b701cc4d39e2d5cd04
s1ddhants1 <siddhant2257@gmail.com>
2025-04-28 22:39:10 +0530

    better text contrast

--- src/lib/components/Valentine.svelte
+++ src/lib/components/Valentine.svelte
@@ -30,13 +30,13 @@ onMount(() => {
 
 <div class="font-[LondonBetween] font-bold text-nowrap flex fixed p-40 z-20 w-full items-center justify-center"> 
   <div class="typing-animation">
-    <p class="text-xl md:text-3xl mt-1 text-white dark:text-white">heyy nini!!</p>
-    <p class="text-xl md:text-3xl mt-1 text-white dark:text-white">would you like to...</p>
+    <p class="text-xl md:text-3xl mt-1 text-black dark:text-white">heyy nini!!</p>
+    <p class="text-xl md:text-3xl mt-1 text-black dark:text-white">would you like to...</p>
     <p class="text-4xl md:text-6xl leading-15 bg-gradient-to-r from-pink-700 via-pink-500 to-pink-200 bg-clip-text text-transparent font-bold">
       Be My Valentine?
     </p>
-    <p class="text-xl md:text-3xl mt-1 text-white dark:text-white">...please?</p>
-    <p class="text-xl md:text-3xl mt-1 text-white dark:text-white">I love you &lt;3</p>
+    <p class="text-xl md:text-3xl mt-1 text-black dark:text-white">...please?</p>
+    <p class="text-xl md:text-3xl mt-1 text-black dark:text-white">I love you &lt;3</p>
   </div>
 </div>
 </section>
