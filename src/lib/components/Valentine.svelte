ab2852d9953967374f311ae84cccbe7668a32146
s1ddhants1 <siddhant2257@gmail.com>
2025-07-27 09:24:49 +0530

    text clip fix

--- src/lib/components/Valentine.svelte
+++ src/lib/components/Valentine.svelte
@@ -42,14 +42,12 @@
 					would you like to...
 				</p>
 				<p
-					class="bg-gradient-to-r from-pink-700 via-pink-500 to-pink-200 bg-clip-text text-4xl leading-10 font-bold text-transparent md:text-6xl"
+					class="bg-gradient-to-r from-pink-700 via-pink-500 to-pink-200 bg-clip-text text-4xl leading-15 font-bold text-transparent md:text-6xl"
 				>
 					Be My Valentine?
 				</p>
-				<p class="mt-0.5 text-xl text-black text-shadow-md md:text-3xl dark:text-white">
-					...please?
-				</p>
-				<p class="mt-0.5 text-xl text-black text-shadow-md md:text-3xl dark:text-white">
+				<p class="mt-1 text-xl text-black text-shadow-md md:text-3xl dark:text-white">...please?</p>
+				<p class="mt-1 text-xl text-black text-shadow-md md:text-3xl dark:text-white">
 					I love you &lt;3
 				</p>
 			</div>
