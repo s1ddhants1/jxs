3aa2273fcbd0913bb42231ad9207ebf590a4fb39
s1ddhants1 <siddhant2257@gmail.com>
2025-08-01 15:31:03 +0530

    fix images pathing add gradient in about

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -23,13 +23,11 @@
 	});
 </script>
 
-<section
-	class="flex min-h-screen w-full flex-col items-center justify-center rounded-xl p-4 dark:bg-gradient-to-br"
->
+<section class="flex min-h-screen w-full flex-col items-center justify-center rounded-xl p-4">
 	<div bind:this={sectionRef} class="relative h-screen w-full overflow-hidden">
 		<!-- Kim slides in from left -->
 		<img
-			src="/kim.webp"
+			src="/imgs/kim.webp"
 			alt="Kim"
 			class="absolute left-0 z-10 mt-10 h-[70vh] max-h-[500px] scale-50 object-contain transition-all duration-[2000ms] ease-in-out md:mt-20 md:max-h-[1000px]"
 			class:translate-x-0={sectionVisible}
@@ -47,18 +45,38 @@
 
 		<!-- Olaf slides in from right -->
 		<img
-			src="/olaf.webp"
+			src="/imgs/olaf.webp"
 			alt="Olaf"
 			class="absolute right-0 z-10 mt-30 h-[70vh] max-h-[500px] scale-50 object-contain transition-all duration-[2000ms] ease-in-out md:mt-20 md:max-h-[1000px]"
 			class:translate-x-0={sectionVisible}
 			class:translate-x-full={!sectionVisible}
 		/>
 	</div>
+	<div class="light absolute min-h-screen w-full items-center justify-center rounded-xl p-4"></div>
 </section>
 
 <style>
 	section {
 		background-size: 200% 200%;
 		animation: gradient 12s ease infinite;
+	}
+
+	.light {
+		background:
+			url('/imgs/grain.webp'),
+			radial-gradient(
+				50% 50% at 50% 50%,
+				rgba(198, 102, 12, 0.742) 0%,
+				rgba(248, 168, 56, 0.2),
+				transparent
+			);
+
+		mask-image: radial-gradient(
+			50% 50% at 50% 50%,
+			white,
+			rgba(0, 0, 0, 1),
+			rgba(0, 0, 0, 1),
+			transparent
+		);
 	}
 </style>
--- src/lib/components/Test.svelte
+++ src/lib/components/Test.svelte
@@ -2,7 +2,7 @@
 	class="relative flex hidden h-full min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-black"
 >
 	<img
-		src="/dino.webp"
+		src="/imgs/dino.webp"
 		alt="dino"
 		class="z-[-10]-10 absolute right-0 mt-10 h-[70vh] max-h-[500px] scale-50 ease-in-out md:mt-50 md:max-h-[1000px]"
 	/>
